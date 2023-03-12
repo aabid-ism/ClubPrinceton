a0c02e34bce7507f66296cfe7dbf7bc8c0d56d47
Christopher Speed <90000903+christopherspeed@users.noreply.github.com>
2023-03-12 15:22:21 -0400

    Post Building Out.1

--- social/src/components/post/Post.js
+++ social/src/components/post/Post.js
@@ -1,15 +1,51 @@
 import React from "react";
 import './Post.css'
+import LOGO from './terry benedict profile pic.png'
 
-function Post({ width }){
+// this should be a separate component for usage in other places maybe?
+function Icon({ image }){
+    return (
+        <div className="postIcon">
+            <img src={image} alt=""/>
+        </div>
+    );
+}
+
+function PostHeader({ props }){
+    return (
+        <div className="postHeader">
+            <Icon image={LOGO}/>
+            <Icon image={LOGO}/>
+            <Icon image={LOGO}/>
+        </div>
+    );
+}
+
+function PostContent({ props }){
+    return (
+        <div className="postContent">
+            <p>Content</p>
+        </div>
+    );
+}
+
+// TODO: Current Idea is put all the rating/interaction into this component/sub-components
+function PostComments({ props }){
+    return (
+        <div className="postComments">
+            <p>Comments</p>
+        </div>
+    );
+}
+
+function Post({ props }){
     return (
         <div className='bubble'>
-            <div>
-                <p>Hello! I'm excited to be here! I like coding, especially
-                    when I can do it instead of something else.
-                </p>
-            </div>
-            
+            <div className='post'>
+                <PostHeader/>
+                <PostContent/>
+                <PostComments/>
+            </div>            
         </div>
     );
 }
