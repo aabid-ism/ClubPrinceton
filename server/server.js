7491498776866d2fedcdba460c99baa18dbf7019
Aabid Ismail <aabid@princeton.edu>
2023-03-13 17:07:41 -0400

    deleted bodyparser and using express.json middleware instead

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const conn = require('./db/conn.js');
 
 // middleware
 app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({
 //     extended: true
 // }));
