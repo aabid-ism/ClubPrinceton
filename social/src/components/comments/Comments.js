925a9d478205c0552d70c0008cfae3c410bafe97
Christopher Speed <90000903+christopherspeed@users.noreply.github.com>
2023-03-15 01:20:27 -0400

    Post and Comments are majority done. Moving on to implement personal comment at post bottom next. Partially refactored post to lessen prop drilling

--- social/src/components/comments/Comments.js
+++ social/src/components/comments/Comments.js
@@ -2,13 +2,12 @@ import Comment from "./Comment";
 import './Comment.css'
 
 export default function Comments({ props }){
-    // const commentInputs = props.commentsData.comments;
     const commentInputText = props.commentsData.comments;
     
     return (
         <div className="comments">
-            {commentInputText.map(text => (
-                <Comment key={{commentText:text}} props={{commentText:text}}></Comment>
+            {commentInputText.map(textObj => (
+                <Comment key={{commentText:textObj.commentText}} props={{commentText:textObj.commentText}}></Comment>
             ))}
         </div>
     );
--- social/src/components/post/Post.js
+++ social/src/components/post/Post.js
@@ -7,23 +7,23 @@ import Comments from "../comments/Comments";
 // this should be a separate component for usage in other places maybe?
 function Icon({ image }){
     return (
-        <div className="postIcon">
+        <div className="headerIcon">
             <img src={image} alt=""/>
         </div>
     );
 }
 
-function HeaderInfo({ props }){
+function HeaderInfo({ children }){
     return (
         <div className="headerContent">
-            <p>Content</p>
+            {children}
         </div>
     );
 }
 
 function OptionButton({ props }){
     return (
-        <div className="postOption">
+        <div className="headerOption">
             <button onClick={() => {
                 alert("Button Clicked!");
             }}>{<FaEllipsisH/>}</button>
@@ -31,15 +31,32 @@ function OptionButton({ props }){
     );
 }
 
-function PostHeader({ props }){
+function PostHeader({ children }){
     return (
         <div className="postHeader">
-            <Icon image={LOGO}/>
-            <HeaderInfo/>
-            <OptionButton/>
+            {children}
         </div>
     );
 }
+
+function PostTitle({ props }){
+    return (
+        <div className="postTitle">
+            {props.title}
+        </div>
+    );
+}
+
+function PostCreationInfo({ props }){
+    return (
+        <div className="postTimelineInfo">
+            <p>{props.creator}</p>
+            <p>{props.createdTime}</p>
+            <p>{props.modTime}</p>
+        </div>
+    );
+}
+
 
 function PostContent({ props }){
     return (
@@ -74,63 +91,75 @@ function PostMetrics({props}) {
 }
 
 // TODO: Create a "Comments" component that takes in and handles all the comment logic
-function PostComments({ props }){
+function PostComments({ children }){
     return (
         <div>
-            <PostMetrics props={props.postMetrics}/>
-            <Comments props={{commentsData: props.commentsData}}/>
-            <div className="your-comment">
-                
-            </div>
+            {children}
         </div>
     );
+}    
+
+function PostBubble({ children }){
+    return (
+        <div className='bubble'>
+            {children}        
+        </div>
+    );
+    
 }
 
 function Post({ props }){
     const defaultPostProps = {
         headerProps: {
             
-        },
+        },    
         contentProps: {
 
-        },
+        },    
         commentsProps:{
             postMetrics:{
                 numberOfPostLikes: 120,
                 numberOfPostComments: 8,
-            },
+            },    
             commentsData: {
                 comments: [
-                    "I love whatever this is!",
-                    "Comment 2",
-                    "Comment 3",
-                    "Comment 4",
-                    "Zoo-Wee-Mama",
-                    "Comment moment",
-                    "Hello",
-                    "More comments hello",
-                    "I love whatever this is!",
-                    "Comment 2",
-                    "Comment 3",
-                    "Comment 4",
-                    "Zoo-Wee-Mama",
-                    "Comment moment",
-                    "Hello",
-                    "More comments hello"
-                ]
-            }
-        }
-    }
+                    {commentText: "I love whatever this is!"},
+                    {commentText: "Comment 2"},
+                    {commentText: "Comment 3"},
+                    {commentText: "Comment 4"},
+                    {commentText: "Zoo-Wee-Mama"},
+                    {commentText: "Comment moment"},
+                    {commentText: "I love whatever this is!1"},
+                    {commentText: "Comment 21"},
+                    {commentText: "Comment 31"},
+                    {commentText: "Comment 41"},
+                    {commentText: "Zoo-Wee-Mama1"},
+                    {commentText: "Comment moment1"}
+                ]    
+            }    
+        }    
+    }    
     return (
-        <div className='bubble'>
-            <div className='post'>
-                <PostHeader/>
-                <PostContent props={{content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla est ullamcorper eget nulla facilisi etiam. Morbi blandit cursus risus at ultrices mi. Curabitur gravida arcu ac tortor. Vestibulum lectus mauris ultrices eros in. Elementum integer enim neque volutpat ac tincidunt vitae semper quis. Sed cras ornare arcu dui vivamus arcu felis. Convallis posuere morbi leo urna molestie at. Massa eget egestas purus viverra accumsan in nisl. Et netus et malesuada fames ac. Amet est placerat in egestas erat imperdiet. Ultrices dui sapien eget mi proin sed. Iaculis nunc sed augue lacus viverra vitae congue eu. Urna duis convallis convallis tellus id. Nibh tortor id aliquet lectus proin nibh. Eu feugiat pretium nibh ipsum consequat nisl vel. Aliquam nulla facilisi cras fermentum odio. Tortor id aliquet lectus proin nibh. Ornare lectus sit amet est placerat. Viverra suspendisse potenti nullam ac tortor vitae. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Feugiat pretium nibh ipsum consequat nisl vel. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Ac feugiat sed lectus vestibulum mattis ullamcorper velit. Faucibus turpis in eu mi bibendum neque egestas."}}/>
-                <PostComments props={defaultPostProps.commentsProps}/>
-            </div>            
-        </div>
+        <PostBubble>
+            <PostHeader>
+                <Icon image={LOGO}/>
+                <HeaderInfo>
+                    <PostTitle props={{title: "Introduction to Posting!"}}/>
+                    <PostCreationInfo props={{creator: "Chris Speed", createdTime:"03/14/23", modTime:"03/15/23"}}/>
+                </HeaderInfo>
+                <OptionButton/>
+            </PostHeader>
+            <PostContent props={{content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla est ullamcorper eget nulla facilisi etiam. Morbi blandit cursus risus at ultrices mi. Curabitur gravida arcu ac tortor. Vestibulum lectus mauris ultrices eros in. Elementum integer enim neque volutpat ac tincidunt vitae semper quis. Sed cras ornare arcu dui vivamus arcu felis. Convallis posuere morbi leo urna molestie at. Massa eget egestas purus viverra accumsan in nisl. Et netus et malesuada fames ac. Amet est placerat in egestas erat imperdiet. Ultrices dui sapien eget mi proin sed. Iaculis nunc sed augue lacus viverra vitae congue eu. Urna duis convallis convallis tellus id. Nibh tortor id aliquet lectus proin nibh. Eu feugiat pretium nibh ipsum consequat nisl vel. Aliquam nulla facilisi cras fermentum odio. Tortor id aliquet lectus proin nibh. Ornare lectus sit amet est placerat. Viverra suspendisse potenti nullam ac tortor vitae. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Feugiat pretium nibh ipsum consequat nisl vel. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Ac feugiat sed lectus vestibulum mattis ullamcorper velit. Faucibus turpis in eu mi bibendum neque egestas."}}/>
+            <PostComments>
+                <PostMetrics props={defaultPostProps.commentsProps.postMetrics}/>
+                <Comments props={{commentsData: defaultPostProps.commentsProps.commentsData}}/>
+                <div className="your-comment">
+                    
+                </div>
+            </PostComments>
+        </PostBubble>                    
     );
-}
+}    
 
 
-  export default Post;
+export default Post;
