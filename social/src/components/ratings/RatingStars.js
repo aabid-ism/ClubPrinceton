da2976ab697542086fec5711eab7300b7075e106
Aditya Palaparthi <avpalaparthi@gmail.com>
2023-03-15 23:11:35 -0400

    modifying stars

--- social/src/components/ratings/RatingStars.js
+++ social/src/components/ratings/RatingStars.js
@@ -3,10 +3,7 @@ import Star from './Star';
 
 const RatingStars = () => {
     const [gradeIndex, setGradeIndex] = useState();
-    const GRADES = ['Poor', 'Fair', 'Good', 'Very good', 'Excellent'];
-    const activeStar = {
-        fill: 'yellow'
-    };
+    const GRADES = ['1', '2', '3', '4', '5'];
 
     const changeGradeIndex = ( index ) => {
         setGradeIndex(index);
@@ -14,16 +11,10 @@ const RatingStars = () => {
 
     return (
         <div className="container">
-            <h1 className="result">{ GRADES[gradeIndex] ? GRADES[gradeIndex] : 'You didn\'t review yet'}</h1>
             <div className="stars">
                 {
                     GRADES.map((grade, index) => (
-                        <Star 
-                            index={index} 
-                            key={grade} 
-                            changeGradeIndex={changeGradeIndex}
-                            style={ gradeIndex > index ? activeStar : {}}
-                        />
+                        <Star />
                     ))
                 }
             </div>
--- social/src/components/ratings/Star.js
+++ social/src/components/ratings/Star.js
@@ -1,35 +1,28 @@
-import React from 'react';
-
-const Star = (props) => {
-
-    const changeGrade = (e) => {
-        props.changeGradeIndex(e.target.value);
-    }
-
+import React, { useState } from 'react';
+// need citation for the polygon dimension
+// remove divs later?
+// should changing background color be dealt with at star or RatingStar level
+// add citations
+function Star(props) {
+    
     return (
-        <label className="star">
-            <input
-                type="radio"
-                name="rating"
-                id={props.grade}
-                value={props.index}
-                className="stars_radio-input"
-                onClick={changeGrade}
-            />
-            <svg 
-                width="58" 
-                height="58" 
-                viewBox="0 0 24 24" 
-                fill="none" 
-                stroke="#393939" 
-                strokeWidth="1" 
-                strokeLinecap="round" 
-                strokeLinejoin="round" 
-                style={props.style}
-            >
-                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-            </svg>
-        </label>
+        <div className='star'>
+                <svg
+                    // changed to 38 from 58
+                    width="28" 
+                    height="58" 
+                    viewBox="0 0 24 24"
+                    stroke="#393939" 
+                    strokeWidth="1.2" 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    style={props.style}
+                    // check to see if you can add this to css
+                    fill="orange"
+                >
+                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+                </svg>
+        </div>
     );
 }
 
