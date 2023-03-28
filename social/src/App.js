8df74a45b8086ac524bd12c906026cd53c52b770
Aabid Ismail <aabid@princeton.edu>
2023-03-28 18:10:12 -0400

    added routers and route in order to allow a different webpage (admin interface) to be able to shown

--- social/src/App.js
+++ social/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import React from "react";
-
 import Landing from "./components/landing/Landing.js";
 import MainBubble from "./components/mainpage/MainPage";
 import SearchBar from "./components/searchBar/SearchBar";
 import HomePage from "./components/homePage/HomePage";
-
 import store from "./store";
 import { Provider } from "react-redux";
 
--- social/src/index.js
+++ social/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./tailwind.css"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
