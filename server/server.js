e762f3ac1f209ed9e4a9a69a940bd7bd11414831
Aditya Palaparthi <avpalaparthi@gmail.com>
2023-03-30 20:36:44 -0400

    one server app successfully built DO NOT PULL INTO MAIN

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const bodyParser = require("body-parser");
 const conn = require('./db/conn.js');
 
 // middleware
-app.use(cors());
+let corsOptions = {
+  origin: "http://localhost:5050"
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({
@@ -25,9 +29,11 @@ app.use((err, _req, res, next) => {
   })
 
 // Defining global routes: localhost:5050/
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
+// removed this code to test one server
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 
 // app.get('/users', (req, res) => {
@@ -37,6 +43,13 @@ app.get('/', (req, res) => {
 // app.get('/users/:id', (req, res) => {
 //   res.send(`User with ID ${req.params.id}`);
 // });
+
+const path = __dirname + '/views/';
+app.use(express.static(path));
+
+app.get('/', function (req, res) {
+  res.sendFile(path + "index.html");
+});
 
 // Start the server
 app.listen(PORT, async () => {
