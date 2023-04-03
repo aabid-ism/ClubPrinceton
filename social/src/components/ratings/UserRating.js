eb0aba5d0379a41a79797d1adc1c100650fb6423
aritraroy0 <aritro.roy0@gmail.com>
2023-04-03 02:16:42 -0400

    Backend of ratings working but frontend has issue updating the rating visually

--- social/src/components/ratings/UserRating.js
+++ social/src/components/ratings/UserRating.js
@@ -1,102 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RatingsBubble from "./RatingsBubble";
 import SingleRating from "./SingleRating";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 
-// To-Do: handle case with constant?
+const url = "http://localhost:5050/ratings";
 
-function UserRating(props) {
-    // To-DO -> values and setData not used
-    // need to preserve the state of the ratings
-    // rerender -> with previous ratings of user if available -> if not set with all zeroes
-    // take adv -> of the initialization stage
-    // do error handling
-    // the question is now -> how do I set data from the server to client side
-    // test on dummy data -> receiving 0,0,0,0 (never rated this club page)
-    // 1, 1, 2, 4
-    // the initial rating
-    // have an update rating/cancel button
-    // have a submit button -> when first seeing it
-    // all zeroes rating -> fetching this -> submit button
-    // other -> update + discard changes
-    // alert for making sure all rows are filled
+function UserRating() {
+  const clubData = useSelector((state) => state.clubData);
 
-    // we may have forgot the folder when pushing to git branch
+  const [data, setData] = useState({
+    Clout: 5,
+    Intensity: 5,
+    Vibes: 5,
+    Inclusivity: 5,
+  });
 
-    // include the useEffect 
-    const [data, setData] = useState({
-        goodVibes: 1,
-        intensity: 2,
-        popularity: 3,
-        inclusivity: 4,
-        clubName: 'Triangle Club'
-      });
+  const dispatch = useDispatch();
 
-    // does every state need to updated like this to pass data from child to parent?
-    // find better alternative later!!! -> once more comfortable with React
-    const storeVibes = (rating) => {
-        setData(previousState => {
-            return { ...previousState, goodVibes: rating }
+  useEffect(() => {
+    if (clubData) {
+      console.log("axios was called clubData.name: " + clubData.name);
+      axios
+        .get(`${url}/${clubData.name}`)
+        .then((response) => {
+          console.log(response.data);
+          setData(response.data);
+          console.log("data was set to: " + data);
+        })
+        .catch((error) => {
+          console.error(error);
         });
-    };
-
-    const storeIntensity = (rating) => {
-        setData(previousState => {
-            return { ...previousState, intensity: rating }
-        });
-    };
-
-    const storePopularity = (rating) => {
-        setData(previousState => {
-            return { ...previousState, popularity: rating }
-        });
-    };
-
-    const storeInclusivity = (rating) => {
-        setData(previousState => {
-            return { ...previousState, inclusivity: rating }
-        });
-    };
-    
-    function handleSubmit(event) {
-    event.preventDefault();
-    alert(JSON.stringify(data, undefined, 2))
     }
-    
-    return (
-        // handle widths?
-        // form attributes?
-        // need to center title
-        // button should change to update rating on click
-        <RatingsBubble>
-            <form 
-                onSubmit={handleSubmit}
-                method="post"
-                className="rtg-form"
-            >
-                <label>Rating</label>
-                <br></br>
-                <label>
-                    <div>Good Vibes</div>
-                    <SingleRating passOnRating={storeVibes} initRating={data.goodVibes}></SingleRating>
-                </label>
-                <br></br>
-                <label>
-                    <div>Intensity</div>
-                    <SingleRating passOnRating={storeIntensity} initRating={data.intensity}></SingleRating>
-                </label>
-                <br></br>
-                <label>
-                    <div>Popularity</div>
-                    <SingleRating passOnRating={storePopularity} initRating={data.popularity}></SingleRating>
-                </label>
-                <br></br>
-                    <div>Inclusivity</div>
-                    <SingleRating passOnRating={storeInclusivity} initRating={data.inclusivity}></SingleRating>
+  }, [clubData]);
 
-                <button type="submit"><strong>Submit Rating</strong></button>
-            </form>
-        </RatingsBubble>
-    );
+  const storeVibes = (rating) => {
+    setData((previousState) => {
+      return { ...previousState, Vibes: rating };
+    });
+  };
+
+  const storeIntensity = (rating) => {
+    setData((previousState) => {
+      return { ...previousState, Intensity: rating };
+    });
+  };
+
+  const storePopularity = (rating) => {
+    setData((previousState) => {
+      return { ...previousState, Clout: rating };
+    });
+  };
+
+  const storeInclusivity = (rating) => {
+    setData((previousState) => {
+      return { ...previousState, Inclusivity: rating };
+    });
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post("/api/ratings", data)
+      .then((response) => {
+        dispatch({
+          type: "SET_CLUB_RATINGS",
+          payload: { id: clubData.id, ratings: data },
+        });
+        // TODO: show success message to the user
+      })
+      .catch((error) => {
+        console.error(error);
+        // TODO: show error message to the user
+      });
+  }
+
+  return (
+    <RatingsBubble>
+      <form onSubmit={handleSubmit} method="post" className="rtg-form">
+        <label>Rating</label>
+        <br></br>
+        <label>
+          <div>Good Vibes</div>
+          <SingleRating
+            passOnRating={storeVibes}
+            initRating={data["Vibes"]}
+          ></SingleRating>
+        </label>
+        <br></br>
+        <label>
+          <div>Intensity</div>
+          <SingleRating
+            passOnRating={storeIntensity}
+            initRating={data["Intensity"]}
+          ></SingleRating>
+        </label>
+        <br></br>
+        <label>
+          <div>Popularity</div>
+          <SingleRating
+            passOnRating={storePopularity}
+            initRating={data["Clout"]}
+          ></SingleRating>
+        </label>
+        <br></br>
+        <div>Inclusivity</div>
+        <SingleRating
+          passOnRating={storeInclusivity}
+          initRating={data["Inclusivity"]}
+        ></SingleRating>
+
+        <button type="submit">
+          <strong>Submit Rating</strong>
+        </button>
+      </form>
+    </RatingsBubble>
+  );
 }
 
 export default UserRating;
