f531aa7a48ede5390cb6fbb2b4cb83f474f484ed
Aditya Palaparthi <avpalaparthi@gmail.com>
2023-04-09 19:27:17 -0400

    add modified app.js

--- social/src/App.js
+++ social/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 import Landing from "./components/landing/Landing.js";
@@ -6,33 +5,21 @@ import MainBubble from "./components/mainpage/MainPage";
 import SearchBar from "./components/searchBar/SearchBar";
 import HomePage from "./components/homePage/HomePage";
 import store from "./store";
+import OvrRtg from "./components/OvrRtg/OvrRtg";
 import { Provider } from "react-redux";
 
 function App() {
   return (
+    // <React.StrictMode>
+    //   <Provider store={store}>
+    //     <HomePage></HomePage>
+    //   </Provider>
+    // </React.StrictMode>
     <React.StrictMode>
       <Provider store={store}>
-        <HomePage></HomePage>
+        <OvrRtg></OvrRtg>
       </Provider>
     </React.StrictMode>
-=======
-import './App.css';
-import Landing from './components/landing/Landing.js';
-import MainBubble from './components/mainpage/MainPage';
-import SearchBar from './components/searchBar/SearchBar';
-import HomePage from './components/homePage/HomePage';
-import Bubble from './components/bubble/Bubble';
-import RatingStars from './components/ratings/SingleRating';
-import SingleRating from './components/ratings/SingleRating';
-import UserRating from './components/ratings/UserRating';
-import OverallRating from './components/ratings/OverallRating';
-
-function App() {
-  return (
-    <div>
-      <OverallRating></OverallRating>
-    </div>
->>>>>>> 92628d23bb98d576060aedf032a309d952de910e
   );
 }
 
