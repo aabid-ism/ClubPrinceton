1f03d3c32a5816cdb3e73092bd4abd73170d0e94
Aditya Palaparthi <avpalaparthi@gmail.com>
2023-04-09 14:04:07 -0400

    new css

--- social/src/App.js
+++ social/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 import Landing from "./components/landing/Landing.js";
@@ -14,6 +15,24 @@ function App() {
         <HomePage></HomePage>
       </Provider>
     </React.StrictMode>
+=======
+import './App.css';
+import Landing from './components/landing/Landing.js';
+import MainBubble from './components/mainpage/MainPage';
+import SearchBar from './components/searchBar/SearchBar';
+import HomePage from './components/homePage/HomePage';
+import Bubble from './components/bubble/Bubble';
+import RatingStars from './components/ratings/SingleRating';
+import SingleRating from './components/ratings/SingleRating';
+import UserRating from './components/ratings/UserRating';
+import OverallRating from './components/ratings/OverallRating';
+
+function App() {
+  return (
+    <div>
+      <OverallRating></OverallRating>
+    </div>
+>>>>>>> 92628d23bb98d576060aedf032a309d952de910e
   );
 }
 
