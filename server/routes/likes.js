8633ce772933afd3c0a47ac36113dcb202c6c7de
Christopher Speed <90000903+christopherspeed@users.noreply.github.com>
2023-04-24 02:14:42 -0400

    Initial liking

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -5,7 +5,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/:commentId', async(req, res) => {
+    console.log("Received comment request!");
+    console.log(req.params.commentId);
+    res.send("Successful!");
+})
 
+router.post('/', async(req, res) => {
+    const { netId, commentId } = req.body;
+    res.send("Hello! Received").status(200);
 })
 
 
--- social/src/components/comments/Comment.js
+++ social/src/components/comments/Comment.js
@@ -1,7 +1,9 @@
 import './Comment.css'
 import { FaRegHeart, FaHeart } from 'react-icons/fa';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import api from '../auth/api';
 
+const url = `${process.env.REACT_APP_SERVER_URL}/likes`;
 // TODO: needs to actually update database with likes
 function Like({ priorLikes, priorLikeStatus }){
     const [isLiked, toggleLike] = useState(priorLikeStatus);
@@ -22,6 +24,22 @@ function Like({ priorLikes, priorLikeStatus }){
 
 // TODO: Make a request for whether the post was liked by a certain user
 export default function Comment({ props }){
+    // TODO 1: How many likes did this comment get?
+    // Send a get request with the comment's id
+    // passdown the initial number of likes
+    // for now
+    const net_id = 'cspeed'
+    let commentLikesData;
+    api
+    .get(`${url}/?netId=${net_id}`)
+    .then((response) => {
+        const data = response.data;
+        console.log(data)
+    })
+    .catch((error) => {
+        console.log("Error occurred: ", error);
+    });
+
     return (
         <div className='comment'>
             <div>
