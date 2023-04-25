c369db2dac07526de6167fa73a8d905b90756f55
Aditya Palaparthi <avpalaparthi@gmail.com>
2023-04-25 01:21:35 -0400

    overall rating fully functional, still working on Breakdown

--- social/src/components/clubRating/ClubRtgBreakdown.js
+++ social/src/components/clubRating/ClubRtgBreakdown.js
@@ -4,6 +4,7 @@ import './clubrating.css';
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+// need to add async/await to allow concurrency
 function BreakdownBubble(props){
     return (
         <div className="rtg-breakdown-bubble">
@@ -14,12 +15,20 @@ function BreakdownBubble(props){
 
 // write this in a more javascript fashion?
 function roundHundreth(value) {
-    return Math.round(value * 100) / 100;
+    return (Math.round(value * 100) / 100).toFixed(2);
 }
 
-function RatingFactor(props) {
-    console.log("Vibes rating: " + props.singleRating);
-    console.log("RGB Colors for red: " + props.rgbColor.red);
+// gets the RGB colors for single rating
+function getRGBColors(singleRating) {
+    const MAX_RTG = 5;
+    const MIN_RTG = 1;
+    const DIFF_RTG = 4;
+    const red = Math.round(255 * (MAX_RTG - singleRating)) / DIFF_RTG;
+    const green = Math.round(255 * (singleRating - MIN_RTG)) / DIFF_RTG;
+    return {red: red, green: green, blue: 0};
+}
+
+function SingleRating(props) {
     let cssProperties = {
         color: "rgb(" + props.rgbColor.red + "," + props.rgbColor.green +
         "," + props.rgbColor.blue +  ")"
@@ -35,75 +44,102 @@ function RatingFactor(props) {
     );
 }
 
+// add asynchronous await -> if needed
 export function ClubRtgBreakdown() {
-    // LATER: BETTER REACT CODE -> UPDATE to USEEFFECT -> do the same for announcements
-    // BUG: RERENDERING MULTIPLE TIMES (5 or 6) instead of just once
     // LATER: More refined coloring system
     const clubData = useSelector(state => state.clubData);
     const clubRating = useSelector(state => state.globalRatings);
     const hasOneUserRtg = useSelector(state => state.hasOneUserRtg);
 
-    console.log("Does this club have at least one user rating? " + hasOneUserRtg);
+    const lightblue = {red: 173, green: 216, blue: 230};
+    const [ratingBreakdown, setRatingBreakdown] = 
+    useState(
+    {
+        vibes: {rating: "N/A", color: lightblue},
+        intensity: {rating: "N/A", color: lightblue},
+        popularity: {rating: "N/A", color: lightblue},
+        inclusivity: {rating: "N/A", color: lightblue}
+    });
 
-    let clubVibes = "NEW";
-    let clubIntensity = "NEW";
-    // clout
-    let clubPopularity = "NEW";
-    let clubInclusivity = "NEW";
+    useEffect(() => {
+        if (clubData.name !== undefined) {
+            if (hasOneUserRtg) {
+                const vibesColor = {};
+                
+
+            }
+            else {
+
+            }
+        }
+
+    }, [clubData, clubRating, hasOneUserRtg]);
+
+    /*
+            clubIntensity = roundHundreth(clubRating.Intensity);
+            clubPopularity = roundHundreth(clubRating.Clout);
+            clubInclusivity
+    */
+
+    // let clubVibes = "NEW";
+    // let clubIntensity = "NEW";
+    // // clout
+    // let clubPopularity = "NEW";
+    // let clubInclusivity = "NEW";
 
     // If club has user ratings -> get RGB colors
-    let getRGBColors = (clubFactor) => {
-        const MAX_RTG = 5;
-        // MIN rating 1 or 0? -> EDGE CASE
-        const MIN_RTG = 1;
-        const DIFF_RTG = 4;
-        const red = Math.round(255 * (MAX_RTG - clubFactor)) / DIFF_RTG;
-        const green = Math.round(255 * (clubFactor - MIN_RTG)) / DIFF_RTG;
+    // let getRGBColors = (clubFactor) => {
+    //     const MAX_RTG = 5;
+    //     // MIN rating 1 or 0? -> EDGE CASE
+    //     const MIN_RTG = 1;
+    //     const DIFF_RTG = 4;
+    //     const red = Math.round(255 * (MAX_RTG - clubFactor)) / DIFF_RTG;
+    //     const green = Math.round(255 * (clubFactor - MIN_RTG)) / DIFF_RTG;
 
-        return {red: red, green: green, blue: 0};
-    };
+    //     return {red: red, green: green, blue: 0};
+    // };
 
     // colors for lightblue background using rgb function
     // can make these consts?
-    let red = 173;
-    let green = 216;
-    let blue = 230;
+    // let red = 173;
+    // let green = 216;
+    // let blue = 230;
 
-    // instead of doing this -> we should have done conditional rendering
-    // this would be the more react way -> change it post beta
-    // using props/ternary operator
+    // // instead of doing this -> we should have done conditional rendering
+    // // this would be the more react way -> change it post beta
+    // // using props/ternary operator
 
-    // see if there is a better way to rewrite this logic
-    let vibesColor = {red: red, green: green, blue: blue};
-    let intensityColor = {red: red, green: green, blue: blue};
-    let popularityColor = {red: red, green: green, blue: blue};
-    let inclusivityColor = {red: red, green: green, blue: blue};
+    // // see if there is a better way to rewrite this logic
+    // let vibesColor = {red: red, green: green, blue: blue};
+    // let intensityColor = {red: red, green: green, blue: blue};
+    // let popularityColor = {red: red, green: green, blue: blue};
+    // let inclusivityColor = {red: red, green: green, blue: blue};
 
-    // { Clout: 0, Vibes: 0, Inclusivity: 0, Intensity: 0 },
-    if (clubData.name !== undefined) {
-        if (hasOneUserRtg) {
-            clubVibes = roundHundreth(clubRating.Vibes);
-            clubIntensity = roundHundreth(clubRating.Intensity);
-            clubPopularity = roundHundreth(clubRating.Clout);
-            clubInclusivity = roundHundreth(clubRating.Inclusivity);
+    // // { Clout: 0, Vibes: 0, Inclusivity: 0, Intensity: 0 },
+    // if (clubData.name !== undefined) {
+    //     if (hasOneUserRtg) {
+    //         clubVibes = roundHundreth(clubRating.Vibes);
+    //         clubIntensity = roundHundreth(clubRating.Intensity);
+    //         clubPopularity = roundHundreth(clubRating.Clout);
+    //         clubInclusivity = roundHundreth(clubRating.Inclusivity);
 
-            vibesColor = getRGBColors(clubVibes);
-            intensityColor = getRGBColors(clubIntensity);
-            popularityColor = getRGBColors(clubPopularity);
-            inclusivityColor = getRGBColors(clubInclusivity);
-        }
-    }
-
-    console.log("VIBES COLORING: " + JSON.stringify(vibesColor));
-
+    //         vibesColor = getRGBColors(clubVibes);
+    //         intensityColor = getRGBColors(clubIntensity);
+    //         popularityColor = getRGBColors(clubPopularity);
+    //         inclusivityColor = getRGBColors(clubInclusivity);
+    //     }
+    // }
 
     return (
         <BreakdownBubble>
             <div>
-                <RatingFactor rgbColor={vibesColor} singleRating={clubVibes} labeling="Good Vibes: "></RatingFactor>
-                <RatingFactor rgbColor={intensityColor} singleRating={clubIntensity} labeling="Intensity: "></RatingFactor>
-                <RatingFactor rgbColor={popularityColor} singleRating={clubPopularity} labeling="Popularity: "></RatingFactor>
-                <RatingFactor rgbColor={inclusivityColor} singleRating={clubInclusivity} labeling="Inclusivity: "></RatingFactor>
+                <h2>Breakdown</h2>
+            </div>
+            <div>
+                <SingleRating rgbColor={vibesColor} singleRating={clubVibes} labeling="Good Vibes: "/>
+                <SingleRating rgbColor={intensityColor} singleRating={clubIntensity} labeling="Intensity: "/>
+                <SingleRating rgbColor={popularityColor} singleRating={clubPopularity} labeling="Popularity: "/>
+                <SingleRating rgbColor={inclusivityColor} singleRating={clubInclusivity} labeling="Inclusivity: "/>
             </div>
         </BreakdownBubble>
     );
--- social/src/components/clubRating/OvrRating.js
+++ social/src/components/clubRating/OvrRating.js
@@ -8,7 +8,6 @@ import { useSelector, useDispatch } from "react-redux";
 
 
 function OvrRtgBubble(props){
-    console.log("this is my props: " + props.redColor);
     let cssProperties = {
         // forget about rgb
         // see if you can pass in red
@@ -32,251 +31,62 @@ function getRGBColors(clubRating) {
     const MAX_RTG = 5;
     const MIN_RTG = 1;
     const DIFF_RTG = MAX_RTG - MIN_RTG;
-    console.log("OzvrRating: Club Rating breakdown: " + JSON.stringify(clubRating));
 
     const clubClout = roundHundreth(clubRating.rating.Clout);
     const clubVibes = roundHundreth(clubRating.rating.Vibes);
     const clubIntensity = roundHundreth(clubRating.rating.Intensity);
     const clubInclusivity = roundHundreth(clubRating.rating.Inclusivity);
 
-    const clubRoundedRtg = `${roundHundreth((clubClout + clubVibes + clubIntensity + clubInclusivity) / 4)}`;
-
-    console.log("club roundedz rating: " + clubRoundedRtg);
-    console.log("type of value for club rounded rating: " + typeof(clubRoundedRtg));
+    const clubRoundedRtg = `${roundHundreth((clubClout + clubVibes + clubIntensity + clubInclusivity) / 4).toFixed(2)}`;
 
     const red = Math.round(255 * (MAX_RTG - clubRoundedRtg)) / DIFF_RTG;
-    console.log("Red color generated: " + red);
     const green = Math.round(255 * (clubRoundedRtg - MIN_RTG)) / DIFF_RTG;
     const blue = 0;
 
-    console.log("redz coloring: " + red);
-
-    console.log("new ratz" + JSON.stringify({clubRoundedRtg: clubRoundedRtg, red: red, green: green, blue: blue}));
-    console.log("please accept: " + clubRoundedRtg);
     return {clubRoundedRtg: clubRoundedRtg, red: red, green: green, blue: blue};
 }
 
 // use useRef
 
 export function OvrRating() {
-    // THE INITIAL VALUE IN USE STATE IS POINTLESS IN CONDITIONAL RENDERING
-    // EDIT: DEBUG LATER IN SEMESTER of why the initial state wasn't being rendered properly
-    // LATER: BETTER REACT CODE -> UPDATE to USEEFFECT -> do the same for announcements
-    // LATER: More refinde coloring system
+    // LATER: More refined coloring system
     const clubData = useSelector(state => state.clubData);
-    // const hasOneUserRtg = useSelector((state) => state.hasOneUserRtg);
     const clubRating = useSelector(state => state.globalRatings);
-    console.log("clubrating for " + clubData.name + " " + JSON.stringify(clubRating));
-    // const [hasClubRating, sethasClubRating] = useState(false);
-    // updates color/number without rerendering component
     const [overallRating, setOverallRating] = useState({clubRoundedRtg: "INITIAL", red: 173, green: 216, blue: 230});
-    // const clubRoundedRtg = useRef("NEW");
-    // const hasClubRating = useRef(false);
     const dispatch = useDispatch();
     const [testBoolean, setTestBoolean] = useState(0);
-    // console.log("state rating: " + JSON.stringify(clubRating));
-    // made it N/A so we can differentiate what is being seen!
-    // in case there is a massive error
-    // something wrong in the axios request
-    // const [ovrRating, setOvrRating] = useState("N/A");
-
-    // runs on only the first render of the component
-    // useEffect(() => {setclubRoundRtg(clubRoundRtg)}, []);
-
-    // use state causes application to rerender -> that's why useffect gets run again
-    // with true value set
-
-    // let hasClubRating = false;
-
 
     const checkUserRtgUrl = `${process.env.REACT_APP_SERVER_URL}/clubRating/check`;
-    // NONE RATING -> GRAY BACKGROUND
-    // BREAKDOWN NONE -> INDIVIDUAL NONE -> GRAY TEXT BACKGROUND
 
-    // to the backend -> we are sending back the club
-    // link we are currently on
-    // async await? necessary?
     useEffect(() => {
-        console.log("first");
-        console.log("I'm in the useEffect for overallRating");
+        // console.log("I'm in the useEffect for overallRating");
         api
             .get(checkUserRtgUrl, {
                 params: {clubName: clubData.name}
             })
             .then((response) => {
                 const hasUserRating= response.data.hasUserRating;
-                console.log("Type of parameter given from endpoint: " + typeof(hasUserRating));
-                console.log("Club has a user rating (1 or 0): " + clubData.name + " " + JSON.stringify(hasUserRating));
-                // setOvrRating(ovrRatingData);
-                // if (hasUserRating) {
-                    // console.log("I have a 0 rating docs yet I'm still getting overall rating")
-                    // this gets set for all of the clubs -? for this component
                     if (hasUserRating === 1) {
-                        setTestBoolean(1);
-                        // setOverallRating({clubRoundedRtg: "test club 2", red: 173, green: 216, blue: 230});
-                        // setOverallRating(getRGBColors(clubRating))
-                        console.log("I'm in has user rating and I'm checking club rating" + JSON.stringify(clubRating));
                         const newClubRating = getRGBColors(clubRating);
                         setOverallRating(newClubRating);
-                        console.log("OVzzzerall rating: " + newClubRating.clubRoundedRtg);
+                        dispatch({
+                            type: "SET_HAS_USER_RATING",
+                            payload: { hasOneUserRtg: true },
+                          });
                     }
                     else {
-                        setTestBoolean(0);
                         setOverallRating({clubRoundedRtg: "NEW", red: 173, green: 216, blue: 230});
+                        dispatch({
+                            type: "SET_HAS_USER_RATING",
+                            payload: { hasOneUserRtg: false },
+                          });
                     }
-                    // setOverallRating({clubRoundedRtg: testBoolean, red: 173, green: 216, blue: 230});
-
-                    // sethasClubRating(true);90
-                    // hasClubRating.current = true;
-                    // dispatch({
-                    //     type: "SET_HAS_USER_RATING",
-                    //     payload: { hasOneUserRtg: true },
-                    // });
-                    // console.log("WE have a club rating elephant: " + hasUserRating);
-                    // const ovrRating = getRGBColors(clubRating);
-                    // setOverallRating(ovrRating);
-                    // write the create club rounded rating in here!
-                // };
             })
             .catch((error) => {
                 console.log("Error occurred: ", error);
             });
 
-    }, [clubData]);
-    // if (clubData.name !== undefined) {
-        // console.log("first");
-        // console.log("I'm in the useEffect for overallRating");
-        // api
-        //     .get(checkUserRtgUrl, {
-        //         params: {clubName: clubData.name}
-        //     })
-        //     .then((response) => {
-        //         const hasUserRating= response.data.hasUserRating;
-        //         console.log("Type of parameter given from endpoint: " + typeof(hasUserRating));
-        //         console.log("Club has a user rating (1 or 0): " + clubData.name + " " + JSON.stringify(hasUserRating));
-        //         // setOvrRating(ovrRatingData);
-        //         // if (hasUserRating) {
-        //             // console.log("I have a 0 rating docs yet I'm still getting overall rating")
-        //             // this gets set for all of the clubs -? for this component
-        //             setTestBoolean(hasUserRating);
-        //             // sethasClubRating(true);90
-        //             // hasClubRating.current = true;
-        //             // dispatch({
-        //             //     type: "SET_HAS_USER_RATING",
-        //             //     payload: { hasOneUserRtg: true },
-        //             // });
-        //             // console.log("WE have a club rating elephant: " + hasUserRating);
-        //             // const ovrRating = getRGBColors(clubRating);
-        //             // setOverallRating(ovrRating);
-        //             // write the create club rounded rating in here!
-        //         // };
-        //     })
-        //     .catch((error) => {
-        //         console.log("Error occurred: ", error);
-        //     });
-    // }
-        // run again if the clubRating from state is updated? Does this work?
-
-    // useEffect(() => {
-    //     console.log("second");
-
-    //     console.log("Do we have a club rating: " + hasClubRating);
-
-    //     if (hasClubRating.current) {
-    //         const MAX_RTG = 5;
-    //         const MIN_RTG = 1;
-    //         const DIFF_RTG = MAX_RTG - MIN_RTG;
-    //         console.log("OvrRating: Club Rating breakdown: " + JSON.stringify(clubRating));
-
-    //         const clubClout = roundHundreth(3);
-    //         const clubVibes = roundHundreth(2.78);
-    //         const clubIntensity = roundHundreth(3.24);
-    //         const clubInclusivity = roundHundreth(1.8);
-
-    //         clubRoundedRtg.current = `${roundHundreth((clubClout + clubVibes + clubIntensity + clubInclusivity) / 4)}`;
-
-    //         console.log("club rounded rating: " + clubRoundedRtg);
-    //         console.log("type of value for club rounded rating: " + typeof(clubRoundedRtg));
-
-    //         const red = Math.round(255 * (MAX_RTG - clubRoundedRtg)) / DIFF_RTG;
-    //         console.log("Red color generated: " + red);
-    //         const green = Math.round(255 * (clubRoundedRtg - MIN_RTG)) / DIFF_RTG;
-    //         const blue = 0;
-
-    //         clubRtgColor.current = {red: red, green: green, blue: blue};
-    //     }
-
-    // }, [clubRating]);
-
-    // // make an axios request using clubData.name to get the overall rating
-    // // Reduce one API call by Final version
-    // // if (clubData.name !== undefined) {
-    // //     api
-    // //         .get(checkUserRtgUrl, {
-    // //             params: {clubName: clubData.name}
-    // //         })
-    // //         .then((response) => {
-    // //             const hasUserRating= response.data.hasUserRating;
-    // //             console.log("Club has a user rating (1 or 0): " + JSON.stringify(hasUserRating));
-    // //             // setOvrRating(ovrRatingData);
-    // //             if (hasUserRating) {
-    // //                 sethasClubRating(true);
-    // //                 dispatch({
-    // //                     type: "SET_HAS_USER_RATING",
-    // //                     payload: { hasOneUserRtg: hasClubRating },
-    // //                 });
-    // //                 console.log("WE have a club rating: " + hasClubRating);
-    // //                 // write the create club rounded rating in here!
-    // //             };
-    // //         })
-    // //         .catch((error) => {
-    // //             console.log("Error occurred: ", error);
-    // //         });
-    // // }
-
-    // // colors for lightblue background using rgb function
-    // let red = 173;
-    // let green = 216;
-    // let blue = 230;
-    // // allow us to tell whether if it entered if/else blocks or not
-    // let clubRoundedRtg = "NEW";
-        
-    // const MAX_RTG = 5;
-    // // MIN rating 1 or 0? -> EDGE CASE
-    // const MIN_RTG = 1;
-    // const DIFF_RTG = 4;
-    // // no need to use redux here -> because calling the endpoint in this function
-    // console.log("Do we have a club rating? " + hasClubRating);
-    //     if (hasClubRating) {
-    //         console.log("Club Rating breakdown: " + JSON.stringify(clubRating));
-    
-    //         // individual factor breakdown rounded to nearest hundreth
-    //         // while the following method propogates error -> users will be able to do
-    //         // the math and verify its correct on the frontend -> they won't see the discrepancies
-    //         const clubClout = roundHundreth(clubRating.Clout);
-    //         const clubVibes = roundHundreth(clubRating.Vibes);
-    //         const clubIntensity = roundHundreth(clubRating.Intensity);
-    //         const clubInclusivity = roundHundreth(clubRating.Inclusivity);
-            
-    //         // 4 = the number of factors in the rating
-    //         clubRoundedRtg = roundHundreth((clubClout + clubVibes + clubIntensity + clubInclusivity) / 4);
-    //         console.log("club rounded rating: " + clubRoundedRtg);
-    //         console.log("type of value for club rounded rating: " + typeof(clubRoundedRtg));
-    //         // setclubRoundRtg("clubRoundRtg");
-    //         // clubRoundedRtg = clubRoundedRtg;
-    
-    //         red = Math.round(255 * (MAX_RTG - clubRoundedRtg)) / DIFF_RTG;
-    //         green = Math.round(255 * (clubRoundedRtg - MIN_RTG)) / DIFF_RTG;
-    //         blue = 0;
-    //     }
-    // // else {
-
-    // //     // setclubRoundRtg("NEW");
-    // // }
-    // console.log("Club rounded rating after conditions: " + clubRoundedRtg);
-    // console.log("new red: " + red);
-
-    // getRGBColors(clubRating);
+    }, [clubData, clubRating]);
 
     // do we capitalize?
     return (
